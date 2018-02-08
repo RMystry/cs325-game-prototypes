c5618c508415488285a7b4ced40efdde9cd1e5f5
Rishabh Mistry <rishabh.mistry@gmail.com>
2018-02-07 22:30:08 -0500

    chagnign names

--- Moving-Around/js/main.js
+++ Moving-Around/js/main.js
@@ -41,12 +41,6 @@ window.onload = function() {
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        // bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
         se.mute = false;
         if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
         {
